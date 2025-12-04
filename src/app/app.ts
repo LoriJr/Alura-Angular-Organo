132be7825003eacb3242383bf4d40bb7c915925c
lorijr <jrsilva.lorivas@gmail.com>
2025-12-04 18:07:41 -0300

    add lista-livros components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from './componentes/rodape/rodape';
-import { Livro } from './componentes/livro/livro';
+import { ListaLivros } from './componentes/lista-livros/lista-livros';
 
 @Component({
   selector: 'app-root',
   imports: [
     Cabecalho,
     Rodape,
-    Livro
+    ListaLivros
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
